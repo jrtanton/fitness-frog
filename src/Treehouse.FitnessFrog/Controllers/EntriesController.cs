9b8cb275c2ea8085db56ba5320e87c09110d866d
Jarrod Tanton <jarrod.tanton@reappdev.com>
2016-10-25 16:38:19 -0400

    Add success alters to index page for add, edit, and delete actions.

--- src/Treehouse.FitnessFrog/Controllers/EntriesController.cs
+++ src/Treehouse.FitnessFrog/Controllers/EntriesController.cs
@@ -69,6 +69,8 @@ namespace Treehouse.FitnessFrog.Controllers
             {
                 _entriesRepository.AddEntry(entry);
 
+                TempData["Message"] = "Your entry was successfully added.";
+
                 return RedirectToAction("Index");
 
             }
@@ -107,6 +109,8 @@ namespace Treehouse.FitnessFrog.Controllers
             {
                 _entriesRepository.UpdateEntry(entry);
 
+                TempData["Message"] = "Your entry was successfully updated.";
+
                 return RedirectToAction("Index");
             }
 
@@ -136,6 +140,8 @@ namespace Treehouse.FitnessFrog.Controllers
         public ActionResult Delete(int id)
         {
             _entriesRepository.DeleteEntry(id);
+
+            TempData["Message"] = "Your entry was successfully deleted.";
 
             return RedirectToAction("Index");
         }
